6c34316cdd92491b7ccd4c505ddd16daf72302ea
judaro26 <131112287+judaro26@users.noreply.github.com>
2025-11-24 15:46:47 -0800

    Update getIpWhitelist.js

--- netlify/functions/getIpWhitelist.js
+++ netlify/functions/getIpWhitelist.js
@@ -1,6 +1,6 @@
-import { ipWhitelist } from './js/utilities/ipWhitelist.js';
+const { ipWhitelist } = require('./js/utilities/ipWhitelist.js');
 
-export const handler = async () => {
+exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({ ipWhitelist })
