24768c26274c092b054ef87d64648a6bd5733cee
judaro26 <131112287+judaro26@users.noreply.github.com>
2026-02-10 10:28:57 -0800

    Update verifyCaptcha.js

--- netlify/functions/verifyCaptcha.js
+++ netlify/functions/verifyCaptcha.js
@@ -4,7 +4,7 @@ const GCP_PROJECT_ID = process.env.GCP_PROJECT_ID;
 const GCP_API_KEY = process.env.GCP_API_KEY;
 
 // ✅ Must match the key used in index.html exactly
-const RECAPTCHA_SITE_KEY = '6LdZFEcsAAAAAHlpQm8kzy0SbkE1uTKuaE9uAo6J';
+const RECAPTCHA_SITE_KEY = 'AIzaSyBx26gUu65CUPbyvPfcWzUbVm4S0K9eeIg';
 const ACTION = 'register';
 
 exports.handler = async (event, context) => {
